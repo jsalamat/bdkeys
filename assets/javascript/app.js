930b67d317e6ff42a89143725a69a7d2398670da
jsalamat <salamat_jon@yahoo.com>
2019-01-15 22:20:41 -0500

    loop elements of keys array and push them to notes arr

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
 // Keys and notes variables store the BD keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
+// looping through the elements of the keys array and pushing them to the notes array
+// passing in the elements by its class name
+keys.forEach(function(key){
+  notes.push(document.getElementById(key));
+})
