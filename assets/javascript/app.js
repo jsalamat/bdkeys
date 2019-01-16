71e66e9edc20f153e5061520e84b60ef7dae7c6c
jsalamat <salamat_jon@yahoo.com>
2019-01-16 11:24:50 -0500

    variable store the bttn progress user thru lyrics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,3 +33,9 @@ const  eventAssignment = function(note) {
 // Loop that runs the array elements through the function
 notes.forEach(eventAssignment);
 // --------------------------------------------
+// These variables store the buttons that progress the user through the lyrics
+let nextOne = document.getElementById('first-next-line');
+let nextTwo = document.getElementById('second-next-line');
+let nextThree = document.getElementById('third-next-line');
+let startOver = document.getElementById('fourth-next-line');
+// --------------------------------------------
